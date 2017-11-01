0046c99b6b690d4efe388b88ea3f9ee1939fb1da
focuschan <1057951706@qq.com>
2017-11-01 11:21:04 +0800

    customize square view

--- app/src/main/java/com/keyu/fight2048/MainActivity.java
+++ app/src/main/java/com/keyu/fight2048/MainActivity.java
@@ -2,13 +2,16 @@ package com.keyu.fight2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SquareView squareView = new SquareView(this, null);
+        ll.addView(squareView);
         System.out.println("hello, world");
     }
 }
---
+++ app/src/main/java/com/keyu/fight2048/SquareView.java
@@ -0,0 +1,26 @@
+package com.keyu.fight2048;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/11/1.
+ */
+
+public class SquareView extends View {
+    private String mText;
+    public SquareView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SquareView, 0, 0);
+        try {
+            mText = typedArray.getString(R.styleable.SquareView_setText);
+        }
+        finally {
+            typedArray.recycle();
+        }
+
+    }
+
+}

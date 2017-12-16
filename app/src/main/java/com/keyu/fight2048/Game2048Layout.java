db2de2d50ce101f10724263db0b6b1df1de38142
focuschan <1057951706@qq.com>
2017-12-16 12:28:04 +0800

    load items for the layout

--- app/src/main/java/com/keyu/fight2048/Game2048Layout.java
+++ app/src/main/java/com/keyu/fight2048/Game2048Layout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.RelativeLayout;
 
 /**
@@ -37,12 +38,35 @@ public class Game2048Layout extends RelativeLayout {
         int wholeSize = Math.min(getMeasuredHeight(), getMeasuredWidth());
         int squareWidth = (wholeSize - 2 * mPadding - (mColumns - 1) * mMargin) / mColumns;
         gameItems = new SquareView[mColumns * mColumns];
-        for (int i = 0; i < mColumns * mColumns; i++) {
-             SquareView square = new SquareView(mContext);
-             gameItems[i] = square;
-             RelativeLayout.LayoutParams lp = new LayoutParams(squareWidth, squareWidth);
-             //不是最后一列，则有右边距
-             //不是第一列
+        if (isFirst) {
+
+            for (int i = 0; i < mColumns * mColumns; i++) {
+                SquareView square = new SquareView(mContext);
+                gameItems[i] = square;
+                square.setId(i + 1);
+                RelativeLayout.LayoutParams lp = new LayoutParams(squareWidth, squareWidth);
+                //不是最后一列，则有右边距
+                if ((i + 1) % mColumns != 0) {
+                    lp.rightMargin = mMargin;
+                }
+                //如果不是第一列，需要设置RIGHT OF
+                if (i % mColumns != 0) {
+                    lp.addRule(RelativeLayout.RIGHT_OF, gameItems[i - 1].getId());
+                }
+                //不是第一行的处理, 要把方块放在上方方块之下
+                if (i / mColumns != 0) {
+                    lp.addRule(RelativeLayout.BELOW, gameItems[i - mColumns].getId());
+                }
+                addView(square, lp);//添加小方块到父控件中
+            }
         }
+        isFirst = false;
+        setMeasuredDimension(wholeSize, wholeSize);//修改大方块的尺寸
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mGestureDetector.onTouchEvent(event);
+        return true;
     }
 }
